2b51200e8bac8eba223caf3baaf00059a0e1e634
Noh-All <kmuszynski15@gmail.com>
2026-04-07 13:08:59 +0200

    Aktualizacja zadania 2.1: dodanie testow dla 2, 4 i 8 watkow

--- zad2.1.c
+++ zad2.1.c
@@ -2,18 +2,30 @@
 #include <stdio.h>
 
 int main(void) {
-    // Pobranie i wypisanie maksymalnej dostępnej liczby wątków
-    printf("Liczba dostepnych watkow: %d\n", omp_get_max_threads());
+    // Pobranie i wypisanie maksymalnej dostepnej liczby watkow
+    printf("Liczba dostepnych watkow: %d\n\n", omp_get_max_threads());
 
+    // Tablica z liczba watkow do przetestowania (wymagane w zadaniu 1)
+    int test_threads[] = {2, 4, 8};
 
-    #pragma omp parallel num_threads(4)
-    {
-        int id = omp_get_thread_num();     //  ID
-        int n = omp_get_num_threads();    // wątki XDDDD
+    // Petla automatycznie uruchamia blok dla 2, 4 i 8 watkow
+    for(int i = 0; i < 3; i++) {
+        int threads = test_threads[i];
+        printf("=== Test dla %d watkow ===\n", threads);
         
-        printf("[Watek %d/%d] Czesc z watku!\n", id, n);
+        // Ustawienie liczby watkow dla biezacego przebiegu
+        omp_set_num_threads(threads);
+
+        // Blok rownolegly (bez twardego num_threads w dyrektywie!)
+        #pragma omp parallel
+        {
+            int id = omp_get_thread_num();     // ID
+            int n = omp_get_num_threads();     // watki
+            
+            printf("[Watek %d/%d] Czesc z watku!\n", id, n);
+        }
+        printf("Koniec bloku rownoleglego dla %d watkow.\n\n", threads);
     }
 
-    printf("Koniec bloku rownoleglego.\n");
     return 0;
 }
